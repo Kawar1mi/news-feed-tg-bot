54c8c87f49a165c2e6dc7732731344b823d930b4
Kawar1mi <grishukov-kirill@mail.ru>
2023-11-17 13:29:38 +0300

    updated migrations

--- internal/storage/migrations/20231115150730_create_sources.sql
+++ internal/storage/migrations/20231115150730_create_sources.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE sources
+CREATE TABLE IF NOT EXISTS sources
 (
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(255) NOT NULL,
@@ -8,9 +8,12 @@ CREATE TABLE sources
     priority   INT          NOT NULL,
     created_at TIMESTAMP    NOT NULL DEFAULT NOW()
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS sources_id_uindex on sources (id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS sources;
+DROP INDEX sources_id_uindex;
 -- +goose StatementEnd
--- internal/storage/migrations/20231115152149_create_articles.sql
+++ internal/storage/migrations/20231115152149_create_articles.sql
@@ -1,22 +1,27 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE articles (
-    id SERIAL PRIMARY KEY,
-    source_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    link VARCHAR(255) NOT NULL,
-    summary TEXT,
-    published_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    posted_at TIMESTAMP,
+CREATE TABLE IF NOT EXISTS articles (
+    id           BIGSERIAL PRIMARY KEY,
+    source_id    BIGINT       NOT NULL,
+    title        VARCHAR(255) NOT NULL,
+    link         TEXT         NOT NULL UNIQUE,
+    summary      TEXT,
+    published_at TIMESTAMP    NOT NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT NOW(),
+    posted_at    TIMESTAMP,
     CONSTRAINT fk_articles_source_id
         FOREIGN KEY (source_id)
             REFERENCES sources (id)
             ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS articles_id_uindex on articles (id);
+CREATE UNIQUE INDEX IF NOT EXISTS articles_source_id_uindex on articles (source_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS articles;
+DROP INDEX articles_id_uindex;
+DROP INDEX articles_source_id_uindex;
 -- +goose StatementEnd
